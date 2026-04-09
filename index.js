e247356654e68a1d394671fc8e23096072f6bee7
Kuldeepch13 <dc629753@gmail.com>
2026-04-09 11:05:26 +0530

    added some css and integrated database

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import path from 'path';
+import { MongoClient } from 'mongodb';
 
 const app = express();
 const publicPath = path.resolve('public')
@@ -7,8 +8,27 @@ app.use(express.static(publicPath))
 
 app.set("view engine", 'ejs')
 
-app.get("/", (req, res)=>{
-    res.render("list");
+
+const dbName="node-project";
+const collectionName="TO-DO";
+const url = "mongodb://localhost:27017"
+const client = new MongoClient(url)
+
+const connection =async ()=>{
+    const connect = await client.connect();
+    return await connect.db(dbName)
+}
+
+app.use(express.urlencoded({extended:false}));
+
+
+
+app.get("/", async(req, res)=>{
+    const db = await connection();
+    const collection = db.collection(collectionName);
+    const result =await collection.find().toArray();
+    console.log(result);
+    res.render("list",{result});
 })
 
 app.get("/add", (req, res)=>{
@@ -19,8 +39,16 @@ app.get("/update", (req, res)=>{
     res.render("update");
 })
 
-app.post("/add", (req, res)=>{
-    res.redirect("/");
+app.post("/add", async(req, res)=>{
+    const db = await connection();
+    const collection = db.collection(collectionName);
+    const result = collection.insertOne(req.body);
+    if(result){
+        res.redirect("/");
+    }else{
+        res.redirect("/add")
+    }
+    
 })
 
 app.post("/update", (req, res)=>{
